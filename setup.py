12a7947ba05ab600505e3d2dba9289cdaac17822
momijiro <82196701+momijiro@users.noreply.github.com>
2023-06-08 08:19:59 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='api_hitter',  # パッケージの名前
-    version='0.3',  # パッケージのバージョン
+    version='0.4',  # パッケージのバージョン
     packages=find_packages(),  # パッケージ内のモジュールやサブパッケージを自動的に見つけてインストール
     install_requires=[  # このパッケージが依存する他のパッケージ
         'requests',
